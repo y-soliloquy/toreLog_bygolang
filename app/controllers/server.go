51e17a390751f49501361415a4e5eb8e1802a918
y-soliloquy <engineer.soliloquy@gmail.com>
2022-05-09 00:42:56 +0900

    共通化したviewの用意とそれを表示するための設定

--- app/controllers/server.go
+++ app/controllers/server.go
@@ -1,9 +1,22 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"text/template"
 	"torelog_bygolang/config"
 )
+
+// view周りの共通化ファイルをまとめる
+func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
+	}
+
+	templates := template.Must(template.ParseFiles(files...))
+	templates.ExecuteTemplate(w, "layout", data)
+}
 
 // サーバーの設定
 func StartMainServer() (err error) {
