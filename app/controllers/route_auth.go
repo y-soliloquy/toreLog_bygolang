d7bea5124eb9c2b5db063c0406bf282166935159
y-soliloquy <engineer.soliloquy@gmail.com>
2022-05-10 00:46:58 +0900

    ユーザー登録画面のGET時とPOST時で処理を分岐させフォームからユーザー登録ができるようにする

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,7 +1,30 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"torelog_bygolang/app/models"
+)
 
 func signup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "navbar", "signup")
+	if r.Method == "GET" {
+		// view表示時
+		generateHTML(w, nil, "layout", "navbar", "signup")
+	} else if r.Method == "POST" {
+		// ユーザー登録時
+		err := r.ParseForm()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		if err := user.CreateUser(); err != nil {
+			log.Fatalln(err)
+		}
+
+		http.Redirect(w, r, "/", 302)
+	}
 }
