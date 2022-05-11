4132a3caa040ea6d2a9ffe7c6e71a5f24abd17c0
y-soliloquy <engineer.soliloquy@gmail.com>
2022-05-11 22:13:18 +0900

    不要コードを削除

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -26,18 +26,14 @@ func index(w http.ResponseWriter, r *http.Request) {
 }
 
 func logout(w http.ResponseWriter, r *http.Request) {
-	println("ログアウトした１")
 	cookie, err := r.Cookie("_cookie")
 	if err != nil {
-		println("ログアウトした２")
 		log.Fatalln(err)
 	}
 	if err != http.ErrNoCookie {
-		println("ログアウトした３")
 		session := models.Session{UUID: cookie.Value}
 		session.DeleteSessionByUUID()
 	}
-	println("ログアウトした４")
 
 	http.Redirect(w, r, "/login", 302)
 }
